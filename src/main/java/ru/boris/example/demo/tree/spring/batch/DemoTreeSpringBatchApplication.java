26272210d83c77e4ea4fe3593d5500bffdc3a635
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2024-02-05 14:59:24 +0300

    init commit

---
+++ src/main/java/ru/boris/example/demo/tree/spring/batch/DemoTreeSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package ru.boris.example.demo.tree.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTreeSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTreeSpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/boris/example/demo/tree/spring/batch/DemoTreeSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boris.example.demo.tree.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTreeSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
